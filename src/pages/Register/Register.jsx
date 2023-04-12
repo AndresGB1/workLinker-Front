5864c45f9354b452371230b8d040c4485c22a845
YilberR <yilrojas@unbosque.edu.co>
2023-04-11 19:44:23 -0500

    cmabioss

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+
 
 export const Register = (props) => {
     const [email, setEmail] = useState('');
@@ -19,58 +19,66 @@ export const Register = (props) => {
     }
 
     return (
-        <div className="auth-form-container">
-          <h2>Registro Nuevo Usuario</h2>
-          <form className="register-form" onSubmit={handleSubmit}>
-            <div className="row">
-              <div className="col-md-6">
-                <div className="form-group">
-                  <label htmlFor="name">Nombres</label>
-                  <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" className="form-control" placeholder="Nombres" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="apellidos">Apellidos</label>
-                  <input value={apellidos} name="apellidos" onChange={(e) => setApellidos(e.target.value)} id="Apellidos" className="form-control" placeholder="Apellidos" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="id">Id</label>
-                  <input value={id} name="id" onChange={(e) => setId(e.target.value)} id="id" className="form-control" placeholder="Id" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="phone">Número de celular</label>
-                  <input value={phone} name="numero" onChange={(e) => setPhone(e.target.value)} id="numero" className="form-control" placeholder="Número de celular" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="country">País</label>
-                  <input value={country} name="country" onChange={(e) => setCountry(e.target.value)} id="country" className="form-control" placeholder="País" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="city">Ciudad</label>
-                  <input value={city} name="city" onChange={(e) => setCity(e.target.value)} id="city" className="form-control" placeholder="Ciudad" />
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="form-group">
-                  <label htmlFor="email">Email</label>
-                  <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" placeholder="correo@dominio.com" id="email" name="email" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="password">Contraseña</label>
-                  <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" className="form-control" placeholder="********" id="password" name="password" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="address">Dirección de residencia</label>
-                  <input value={address} onChange={(e) => setAddress(e.target.value)} type="text" className="form-control" placeholder="Dirección de residencia" id="address" name="address" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="description">Descripción</label>
-                  <textarea value={description} onChange={(e) => setDescription(e.target.value)} className="form-control" placeholder="Escribe algo sobre ti" id="description" name="description" rows="5"></textarea>
-                </div>
-              </div>
+    <div className="auth-form-container">
+    <h2>Registro Nuevo Usuario</h2>
+    <form className="register-form" onSubmit={handleSubmit}>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            <div style={{ width: '50%', display: 'inline-block' }}>
+                <label htmlFor="name">Nombres</label>
+                <br></br>
+                <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Nombres" />
             </div>
-            <button type="submit" className="btn btn-primary">Registrarse</button>
-          </form>
+            <div style={{ width: '50%', display: 'inline-block' }}>
+                <label htmlFor="apellidos">Apellidos</label>
+                <br></br>
+                <input value={apellidos} name="apellidos" onChange={(e) => setApellidos(e.target.value)} id="Apellidos"placeholder="Apellidos" />
+            </div>
+            <div style={{ width: '50%', display: 'inline-block' }}>
+                <label htmlFor="id">Id</label>
+                <br></br>
+                <input value={id} name="id" onChange={(e) => setId(e.target.value)} id="id" placeholder="Id" />
+            </div>
+            <div style={{ width: '50%', display: 'inline-block' }}>
+                <label htmlFor="phone">Número de celular</label>
+                <br></br>
+                <input value={phone} name="numero" onChange={(e) => setPhone(e.target.value)} id="numero" placeholder="Número de celular" />
+            </div>
+            <div style={{ width: '50%', display: 'inline-block' }}>
+                <label htmlFor="country">País</label>
+                <br></br>
+                <input value={country} name="country" onChange={(e) => setCountry(e.target.value)} id="country" placeholder="País" />
+            </div>
+            <div style={{ width: '50%', display: 'inline-block' }}>
+                <label htmlFor="city">Ciudad</label>
+                <br></br>
+                <input value={city} name="city" onChange={(e) => setCity(e.target.value)} id="city" placeholder="Ciuadad" />
+            </div>
+            <div style={{ width: '50%', display: 'inline-block' }}>
+                <label htmlFor="email">Email</label>
+                <br></br>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="correo@dominio.com" id="email" name="email" />
+            </div>
+            <div style={{ width: '50%', display: 'inline-block' }}>
+                <label htmlFor="password">Contraseña</label>
+                <br></br>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            </div>
+            <div style={{ width: '50%', display: 'inline-block' }}>
+                <label htmlFor="address">Dirección de residencia</label>
+                <br></br>
+                <input value={address} onChange={(e) => setAddress(e.target.value)}type="address" placeholder="" id="address" name="address" />
+            </div>
+            <div style={{ width: '50%', display: 'inline-block' }}>
+                <label htmlFor="password">Contraseña</label>
+                <br></br>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            </div>
+        </div>
+        <br></br>
+        <button type="submit">Log In</button>
+    </form>
     <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Ya tienes una cuenta? Ingresa ahora.</button>
 </div>
+
     )
 }
