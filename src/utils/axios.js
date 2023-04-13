95d29668faae5414acc362ad4800190664ed055f
Andres Galvis B <a.galvisbolivar@gmail.com>
2023-04-13 16:41:33 -0500

    feat: add routes

---
+++ src/utils/axios.js
@@ -0,0 +1,12 @@
+const rutas = {
+    host: 'http://localhost:8080/ws',
+    user: '/user',
+    login: '/login',
+    all: '/all',
+    add: '/add',
+    update: '/update/',
+    delete: '/delete/',
+    get: '/get/',
+}
+
+export default rutas;
